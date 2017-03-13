976f605d0af6f56020f29bce4fbababae23d6ff5
dariaamir <zelenayakoshka@yandex.ru>
2017-03-13 23:37:39 +0300

    -edited script - added analyze for each line of logs

--- src/lib/log_parser.py
+++ src/lib/log_parser.py
@@ -1,79 +1,102 @@
-'''
-INPUT TYPE : command line
-INPUT:
-the name or id of a virtual machine
-starting time
-end time
-log file name
-
-OUTPUT TYPE: standart output
-OUTPUT:
-important events related to the virtual machine (action: start, stop, migrate, error ...)
-final state of the virtual machine at the end time (memory)
-'''
 import re
-import sys
-from time import strptime
+from time import strptime, strftime
 from file_read_backwards import FileReadBackwards
 
 
 class LogParser(object):
-
-    vm_names = {'centos-server-text': '43f02a2d-e563-4f11-a7bc-9ee191cfeba1',
-                'debian': 'dd18b0d2-ecd0-43dc-9f86-7e1219780967'}
     engine_log_file = 'engine.log'
     engine_log_address = '../ovirt_logs/' + engine_log_file
     read_engine_log = FileReadBackwards(engine_log_address, encoding="utf-8")
     file_name = ''
     file_address = '../ovirt_logs/' + file_name
 
-    def main(self):
-        pass
-
     def search(self):
-        vm_name = sys.argv[1]
-        start_time = strptime(sys.argv[2] + ' ' + sys.argv[3], '%Y-%m-%d %H:%M:%S')
-        end_time = strptime(sys.argv[4] + ' ' + sys.argv[5], '%Y-%m-%d %H:%M:%S')
-        file_name = sys.argv[6]
-        self.file_address = '../ovirt_logs/' + file_name
+        self.file_address = '../ovirt_logs/' + self.file_name
+        vm_names = self.get_all_vm_active()
+        vm_id = vm_names[self.vm_name]
 
         for line in self.read_engine_log:
             if re.match(r'^[0-9]{4}', line):
                 log_time = strptime(line[0:19], '%Y-%m-%d %H:%M:%S')
-                if log_time < end_time:
-                    if vm_name in line:
-                        print(line)
+                if log_time < self.end_time and vm_id in line:
+                        self.search_for_vm_data_in_engine_log(line, log_time)
+        try:
+            with open(self.file_address, 'r') as vm_file_log:
+                for line in vm_file_log:
+                    if re.match(r'^[0-9]{4}', line):
+                        log_time = strptime(line[0:19],'%Y-%m-%d %H:%M:%S')
+                        if self.start_time<= log_time <= self.end_time and vm_id in line:
+                            self.search_for_vm_data_in_vm_log(line, log_time)
+                        elif log_time >= self.end_time:
+                            break
+        except FileNotFoundError:
+            print('Log file %s not found' % self.file_name)
 
-        with open(self.file_address, 'r') as vm_file_log:
-            for line in vm_file_log:
-                if re.match(r'^[0-9]{4}', line):
-                    log_time = strptime(line[0:19],'%Y-%m-%d %H:%M:%S')
-                    if start_time<= log_time <= end_time:
-                        self.search_for_vm_data(line, vm_name)
-                    elif log_time >= end_time:
-                        break
+    def search_for_vm_data_in_vm_log(self, line, log_time):
+        print_log_time = strftime('%Y-%m-%d %H:%M:%S', log_time)
+        if ' WARN ' in line:
+            print(print_log_time, re.search(r'WARN.+', line).group(0))
+        elif ' ERROR ' in line:
+            print(print_log_time, re.search(r'ERROR.+', line).group(0))
+        elif ' INFO ' in line:
+            if len(line) <= 300:
+                print(print_log_time, re.search(r'INFO.+', line).group(0))
+            elif 'START' in line:
+                print(print_log_time,
+                      'INFO',
+                      re.search(r'START \S+', line).group(0))
+            elif ' FINISH ' in line:
+                print(print_log_time,
+                      'INFO',
+                      re.search(r'FINISH \S+', line).group(0))
 
-    def search_for_vm_data(self, line, vm_name):
-        if vm_name in line:
-            print(line)
+    def search_for_vm_data_in_engine_log(self, line, log_time):
+        print_log_time = strftime('%Y-%m-%d %H:%M:%S', log_time)
+        logged_actions = [
+            'moved.*',              # VM moved from one state to another
+            'Running command.*'     # Command is run on VM
+        ]
+        for command in logged_actions:
+            if re.search(command, line):
+                print(print_log_time, re.search(command, line).group(0))
+            elif re.search(r'.+FullListVDSCommand.+FINISH.+', line):
+                print(print_log_time,
+                      'VM state:',
+                      re.search(r'emulatedMachine=\S+', line).group(0),
+                      re.search(r'vmType=\S+', line).group(0),
+                      re.search(r'memSize=\S+', line).group(0),
+                      re.search(r'status=\S+', line).group(0),
+                      re.search(r'displayNetwork=\S+', line).group(0),
+                      sep='\n\t')
+            elif re.search(r'.+ ERROR .+', line):
+                if 'VmsMonitoring' in line:
+                    print(print_log_time,
+                          'ERROR VmsMonitoring',
+                          re.search(r'Rerun.+ ', line).group(0))
+                else:
+                    print(print_log_time,
+                          'ERROR',
+                          re.search(r'\'\w+', line).group(0).replace('\'', ''),
+                          re.search(r'execution failed:.+', line).group(0))
 
-    def get_all_vm_active_by_time(self):
-        end_time = strptime(sys.argv[1] + ' ' + sys.argv[2], '%Y-%m-%d %H:%M:%S')
+    def get_all_vm_active(self):
         vm_dict = {}
         for line in self.read_engine_log:
             if re.match(r'^[0-9]{4}', line):
                 log_time = strptime(line[0:19], '%Y-%m-%d %H:%M:%S')
-                if log_time <= end_time and 'FullListVDSCommand' in line:
+                if log_time <= self.end_time and 'FullListVDSCommand' in line:
                     vm_id_search = re.search(r'vmId=\'[\w|-]*\'',line)
-                    if vm_id_search != None:
+                    if vm_id_search:
                         vm_id = vm_id_search.group(0)[6:-1]
                     vm_name_search = re.search(r'vmName=[\w|-]*',line)
-                    if vm_name_search != None:
+                    if vm_name_search:
                         vm_name = vm_name_search.group(0)[7:]
                     if vm_name not in vm_dict:
                         vm_dict[vm_name] = vm_id
         return vm_dict
 
-    if __name__ == '__main__':
-        main()
-
+    def __init__(self, vm_name, start_time, end_time, file_name):
+        self.vm_name = vm_name
+        self.start_time = start_time
+        self.end_time = end_time
+        self.file_name = file_name
--- src/scripts/log_search.py
+++ src/scripts/log_search.py
@@ -1,6 +1,21 @@
 import sys
-sys.path.append("../lib")
-import log_parser
+from lib import log_parser
+from time import strptime
 
-s = log_parser.LogParser()
-s.search()
+
+def parse_arguments(arguments):
+    vm_name = arguments[1]
+    start_time = strptime(arguments[2] + ' ' + arguments[3], '%Y-%m-%d %H:%M:%S')
+    end_time = strptime(arguments[4] + ' ' + arguments[5], '%Y-%m-%d %H:%M:%S')
+    file_name = arguments[6]
+    return vm_name, start_time, end_time, file_name
+
+
+def main(arguments):
+    kwargs = parse_arguments(arguments)
+    s = log_parser.LogParser(*kwargs)
+    s.search()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
