25536138893a6d82850e1543234247812c202af7
Анастасия Анциферова <anastasiia.antsiferova@graphics.cs.msu.ru>
2017-03-18 16:10:14 +0300

    Visualizaion for errors statistics added
    Generating dynamic plots for errors statistics, serialization to json format

---
+++ src/stat_to_plot.js
@@ -0,0 +1,432 @@
+var errors_data = 
+{
+    "engine": {
+        "1487842110": {
+            "_EE-ManagedThreadFactory-default-Thread-1_": 2
+        },
+        "1487842214": {
+            "_ForkJoinPool-1-worker-4_": 10
+        },
+        "1487842410": {
+            "_EE-ManagedThreadFactory-default-Thread-4_": 2
+        },
+        "1487842710": {
+            "_EE-ManagedThreadFactory-default-Thread-4_": 2
+        },
+        "1487843010": {
+            "_EE-ManagedThreadFactory-default-Thread-1_": 2
+        },
+        "1487843019": {
+            "_VdsDeploy_": 4,
+            "_pool-7-thread-1_": 16
+        },
+        "1487843020": {
+            "_VdsDeploy_": 4
+        },
+        "1487843021": {
+            "_pool-7-thread-2_": 16
+        },
+        "1487843310": {
+            "_EE-ManagedThreadFactory-default-Thread-5_": 2
+        },
+        "1487843328": {
+            "_ForkJoinPool-1-worker-8_": 3
+        },
+        "1487843329": {
+            "_ForkJoinPool-1-worker-8_": 2
+        },
+        "1487843458": {
+            "_ForkJoinPool-1-worker-3_": 10
+        },
+        "1487843610": {
+            "_EE-ManagedThreadFactory-default-Thread-5_": 2
+        },
+        "1487843662": {
+            "_ForkJoinPool-1-worker-5_": 10
+        },
+        "1487843882": {
+            "_DefaultQuartzScheduler2_": 2
+        },
+        "1487843910": {
+            "_EE-ManagedThreadFactory-default-Thread-3_": 2
+        },
+        "1487844210": {
+            "_EE-ManagedThreadFactory-default-Thread-3_": 2
+        },
+        "1487844510": {
+            "_EE-ManagedThreadFactory-default-Thread-3_": 2
+        },
+        "1487844762": {
+            "_default_task-101_": 12
+        },
+        "1487844810": {
+            "_EE-ManagedThreadFactory-default-Thread-3_": 2
+        },
+        "1487844939": {
+            "_ForkJoinPool-1-worker-0_": 10
+        },
+        "1487845110": {
+            "_EE-ManagedThreadFactory-default-Thread-4_": 2
+        },
+        "1487845111": {
+            "_ForkJoinPool-1-worker-2_": 10
+        }
+    },
+    "vdsm_1": {
+        "1487844348": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844349": {
+            "_storage.Dispatcher_": 2,
+            "_storage.TaskManager.Task_": 2
+        },
+        "1487844359": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844371": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844381": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844391": {
+            "_storage.Dispatcher_": 2,
+            "_storage.TaskManager.Task_": 2
+        },
+        "1487844401": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844402": {
+            "_storage.Dispatcher_": 2,
+            "_storage.TaskManager.Task_": 2
+        },
+        "1487844412": {
+            "_storage.Dispatcher_": 8,
+            "_storage.TaskManager.Task_": 8
+        },
+        "1487844422": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844432": {
+            "_storage.Dispatcher_": 2,
+            "_storage.TaskManager.Task_": 2
+        },
+        "1487844442": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844443": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844453": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844463": {
+            "_storage.Dispatcher_": 2,
+            "_storage.TaskManager.Task_": 2
+        },
+        "1487844473": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844474": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844484": {
+            "_storage.Dispatcher_": 2,
+            "_storage.TaskManager.Task_": 2
+        },
+        "1487844494": {
+            "_storage.Dispatcher_": 6,
+            "_storage.TaskManager.Task_": 6
+        },
+        "1487844504": {
+            "_storage.Dispatcher_": 8,
+            "_storage.TaskManager.Task_": 8
+        },
+        "1487844515": {
+            "_storage.Dispatcher_": 2,
+            "_storage.TaskManager.Task_": 2
+        },
+        "1487844525": {
+            "_storage.Dispatcher_": 8,
+            "_storage.TaskManager.Task_": 8
+        },
+        "1487844535": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844545": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844556": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844566": {
+            "_storage.Dispatcher_": 2,
+            "_storage.TaskManager.Task_": 2
+        },
+        "1487844576": {
+            "_storage.Dispatcher_": 6,
+            "_storage.TaskManager.Task_": 6
+        },
+        "1487844586": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844587": {
+            "_storage.Dispatcher_": 2,
+            "_storage.TaskManager.Task_": 2
+        },
+        "1487844597": {
+            "_storage.Dispatcher_": 8,
+            "_storage.TaskManager.Task_": 8
+        },
+        "1487844607": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844617": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844627": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844637": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844647": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844657": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844667": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844677": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844687": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844697": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844707": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844717": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844728": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844738": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844748": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844758": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844768": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844769": {
+            "_storage.StoragePool_Backup_domain_validation_failed_": 1,
+            "_storage.StoragePool_Error_producing_domain_dd7732a3-44fc-4d08-bd5d-1a06273580fc,_ignoring_": 1,
+            "_storage.StoragePool_Error_producing_domain_ed2199d0-0125-480e-85f0-04e9a3548abe,_ignoring_": 1,
+            "_storage.StoragePool_FINISH_thread_": 4
+        },
+        "1487844778": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844788": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844798": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844808": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844818": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844828": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844838": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844848": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844858": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844868": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844878": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844888": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487844898": {
+            "_storage.Monitor_Setting_up_monitor_for_dd7732a3-44fc-4d08-bd5d-1a06273580fc_failed_": 1,
+            "_storage.Monitor_Setting_up_monitor_for_ed2199d0-0125-480e-85f0-04e9a3548abe_failed_": 1
+        },
+        "1487845326": {
+            "_virt.vm_": 4
+        },
+        "1487845379": {
+            "_virt.vm_": 4
+        },
+        "1487845723": {
+            "_storage.StoragePool_FINISH_thread_": 2
+        },
+        "1487846929": {
+            "_storage.Dispatcher_": 2,
+            "_storage.TaskManager.Task_": 2,
+            "_vds_Error_restoring_VM_parameters_": 1,
+            "_virt.vm_": 2
+        },
+        "1487847476": {
+            "_virt.vm_": 4
+        },
+        "1487847533": {
+            "_virt.vm_": 4
+        },
+        "1487847769": {
+            "_jsonrpc.JsonRpcServer_Internal_server_error_": 1
+        },
+        "1487848363": {
+            "_virt.vm_": 2
+        }
+    },
+    "vdsm_2": {
+        "1487844453": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 2
+        },
+        "1487844463": {
+            "_storage.Dispatcher_": 8,
+            "_storage.TaskManager.Task_": 8
+        },
+        "1487844474": {
+            "_storage.Dispatcher_": 2,
+            "_storage.TaskManager.Task_": 2
+        },
+        "1487844484": {
+            "_storage.Dispatcher_": 8,
+            "_storage.TaskManager.Task_": 8
+        },
+        "1487844494": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844504": {
+            "_storage.Dispatcher_": 2,
+            "_storage.TaskManager.Task_": 2
+        },
+        "1487844514": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844515": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844525": {
+            "_storage.Dispatcher_": 2,
+            "_storage.TaskManager.Task_": 2
+        },
+        "1487844535": {
+            "_storage.Dispatcher_": 6,
+            "_storage.TaskManager.Task_": 6
+        },
+        "1487844545": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844546": {
+            "_storage.Dispatcher_": 2,
+            "_storage.TaskManager.Task_": 2
+        },
+        "1487844556": {
+            "_storage.Dispatcher_": 6,
+            "_storage.TaskManager.Task_": 6
+        },
+        "1487844566": {
+            "_storage.Dispatcher_": 8,
+            "_storage.TaskManager.Task_": 8
+        },
+        "1487844576": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844587": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487844597": {
+            "_storage.Dispatcher_": 2,
+            "_storage.TaskManager.Task_": 2
+        },
+        "1487844607": {
+            "_storage.Dispatcher_": 4,
+            "_storage.TaskManager.Task_": 4
+        },
+        "1487847534": {
+            "_root_FINISH_thread_": 2,
+            "_virt.vm_": 3
+        },
+        "1487847935": {
+            "_root_FINISH_thread_": 2,
+            "_virt.vm_": 3
+        }
+    }
+};
--- src/summarize_logs.py
+++ src/summarize_logs.py
@@ -4,22 +4,27 @@ import argparse
 import re
 import dateutil.parser
 import pytz
+import json
+import time
 from pytz import timezone
 from datetime import datetime, date, time
 
 def DatetimeParser(date_time):
+	epoch = datetime.utcfromtimestamp(0)
 	error_date = date_time.partition(" ")[0]
 	error_rest = date_time.partition(" ")[2]
 	error_time = error_rest.partition(",")[0]
 	
-	error_tz = error_rest.partition(",")[2][3:]
+	#error_tz = error_rest.partition(",")[2][3:]
 	try:
 		#print(date_time.partition(",")[0] + " " + date_time.partition(",")[2][3:])
-		dt = datetime.strptime(date_time.partition(",")[0] + date_time.partition(",")[2][3:], "%Y-%m-%d %H:%M:%S %Z").replace(tzinfo=pytz.utc)
+		#dt = datetime.strptime(date_time.partition(",")[0] + date_time.partition(",")[2][3:], "%Y-%m-%d %H:%M:%S %Z").replace(tzinfo=pytz.utc)
+		dt = datetime.strptime(date_time.partition(",")[0], "%Y-%m-%d %H:%M:%S")
+		dt = str(int((dt - epoch).total_seconds()))
 	except ValueError:
 		pass
 		print(date_time, ': Unknown format')
-	return (error_date, error_time)
+	return dt
 
 def MessageParser(message):
 	objects = ['SQL', 'SSH', 'host']
@@ -36,7 +41,7 @@ def MessageParser(message):
 		msg_text += [text_found]
 	return msg_text, msg_obj
 
-def SummarizeErrors(error_text):
+def SummarizeErrors(error_text, error_time):
 	errors_dict = {}
 	for err in error_text:
 		if not(err[0] in errors_dict.keys()):
@@ -46,6 +51,22 @@ def SummarizeErrors(error_text):
 				errors_dict[err[0]][err[ms_idx]] += 1
 			else:
 				errors_dict[err[0]][err[ms_idx]] = 1
+	return errors_dict
+
+def SummarizeErrorsTimeFirst(error_text, error_time):
+	error_idx_by_time = sorted(range(len(error_time)), key = lambda k: error_time[k])
+	error_text = list(np.asarray(error_text)[error_idx_by_time])
+	error_time = list(np.asarray(error_time)[error_idx_by_time])
+
+	errors_dict = {}
+	for err_idx, err_time in enumerate(error_time):
+		if not(err_time in errors_dict.keys()):
+			errors_dict[err_time] = {}
+		for ms_idx in range(len(error_text[err_idx])):
+			if not(error_text[err_idx][0] in errors_dict[err_time].keys()):
+				errors_dict[err_time][error_text[err_idx][0]] = 1
+			else:
+				errors_dict[err_time][error_text[err_idx][0]] += 1
 	return errors_dict
 
 def PrintStatistics(logfile_name, errors_dict, errors_num):
@@ -59,46 +80,68 @@ def PrintStatistics(logfile_name, errors_dict, errors_num):
 		for j in errors_dict[i].keys():
 			err_info += "%8d| %s\n" % (errors_dict[i][j], j)
 		err_info += '----------------------------------------\n'
-	#print(err_info)
 	return err_info
+
+def DumpJson(log_names, all_errors):
+	all_data = {}
+	with open('stat_to_plot.js', 'w') as outfile:
+		outfile.write('var errors_data = \n')
+		for log_idx, logname in enumerate(log_names):
+			logname = re.sub('[+\-\s,]', '_', logname)
+			all_data[logname] = all_errors[log_idx]
+		json.dump(all_data, outfile, indent=4, sort_keys=True)
+		outfile.write(';\n')
+		outfile.close()
 
 def main():
 	parser = argparse.ArgumentParser(description='Parse logfiles and summarize errors into standart output.')
 	parser.add_argument('log_directory', metavar='directory', type=str, nargs=1, help='logfiles directory')
-	parser.add_argument('log_filenames', metavar='filename', type=str, nargs='+', help='logfiles filenames (with expansion)')
-	parser.add_argument("-o", "--output", help="Directs the output to a name of your choice")
+	parser.add_argument('log_filenames', metavar='filename', type=str, nargs='+', help='logfiles filenames (without expansion)')
+	parser.add_argument("-o", "--output", help="Directs the output to a name of your choice (with expansion)")
+	parser.add_argument("-js", "--json", action='store_true', help="Create js file with errors statistics in json")
 	args = parser.parse_args()
-	
+
+	if args.json:
+		all_errors_to_dump = []
+
 	output_text = ''
 	for log in args.log_filenames:
 		#error_date = []
-		#error_time = []
+		error_time = []
 		error_msg_text = []
 		error_msg_obj = []
-		print('Analysing', args.log_directory[0] + '/' + log, '...')
-		if not os.path.isfile(args.log_directory[0] + '/' + log):
-			print("File not found: %s\n" % log)
+		print('Analysing', args.log_directory[0] + '/' + log + '.log', '...')
+		if not os.path.isfile(args.log_directory[0] + '/' + log + '.log'):
+			print("File not found: %s\n" % log+ '.log')
 			continue
 
-		with open(args.log_directory[0] + '/' + log) as f:
+		with open(args.log_directory[0] + '/' + log + '.log') as f:
 			for line in f:
 				if 'ERROR' in line:
-					#error_datetime = line.partition(" ERROR")[0]
-					#error_date_and_time = DatetimeParser(error_datetime)
+					error_datetime = line.partition(" ERROR")[0]
+					error_date_and_time = DatetimeParser(error_datetime)
 					#error_date += [error_date_and_time[0]]
-					#error_time += [error_date_and_time[1]]
+					error_time += [error_date_and_time]
 					error_msg_info = MessageParser(line.partition(" ERROR ")[2])
 					error_msg_text += [error_msg_info[0]]
 					error_msg_obj += [error_msg_info[1]]
-	
-		summarize_errors = SummarizeErrors(error_msg_text)	
+
+		summarize_errors = SummarizeErrors(error_msg_text, error_time)
 		output_text += PrintStatistics(log, summarize_errors, len(error_msg_text))
+
+		if args.json:
+			summarize_errors_for_json = SummarizeErrorsTimeFirst(error_msg_text, error_time)
+			output_text += PrintStatistics(log, summarize_errors_for_json, len(error_msg_text))
+			all_errors_to_dump += [summarize_errors_for_json]
+	
+	if args.json:
+		DumpJson(args.log_filenames, all_errors_to_dump)
 	
 	if args.output is not None:
 		output_file = open(args.output, 'w')
- 		#output_file.write("%s:\n" % log)
 		output_file.write(output_text)
 		output_file.close()
 	else:
+		#print('Here will be output text')
 		print(output_text)	
 main()
